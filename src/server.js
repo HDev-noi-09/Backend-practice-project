4299f8a7b048c4fe13375f73baa0cf78ebf8c343
Himaghna <himuraja2006@gmail.com>
2025-12-14 17:53:26 +0530

    commit 2

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+import {DB_NAME} from './constants.js';
+import dotenv from 'dotenv';
+
+
+dotenv.config({
+    quiet:true,
+    path:'./.env'
+});
+
+import express from 'express'
+import connectDB from './db/server.connect.js';
+
+
+const app=express()
+// ( async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+
+//        app.on("error",(error)=>{
+//         console.log("ERROR",error);
+//             throw error
+//        })
+
+//        app.listen(process.env.PORT , () =>{
+//         console.log(`App is listening on port ${process.env.PORT}`);
+//        })
+//     } catch (error) {
+//         console.error("Error:",error);
+//         throw err
+//     }
+// })()
+connectDB();
