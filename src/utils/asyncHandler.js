6df9b022ed13d9300f31981c008c212774e017c3
Himaghna <himuraja2006@gmail.com>
2025-12-16 11:58:40 +0530

    utils completed

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+const asyncHandler=(requestHandler)=>
+    (req,res,next) =>{
+        Promise.resolve(requestHandler(req,res,next)).catch(next)
+    }
+
+
+export {asyncHandler}
+
+
+// const asyncHandler = (func) => async (req,res,next)=>{
+// try {
+//     await func(req,res,next)
+// } catch (error) {
+//     res.status(error.code || 500).json({success:false},{message:error.message})
+// }
+// }
